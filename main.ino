08fa27333626cd4bdf783cf89dd56bdc647a3c48
Daniel Vrátil <me@dvratil.cz>
2021-05-10 14:12:54 +0200

    Initial commit

---
+++ main.ino
@@ -0,0 +1,17 @@
+/*
+* SPDX-FileCopyrightText: 2021 Daniel Vrátil <me@dvratil.cz>
+*
+* SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#include <SoftwareSerial.h>
+
+#include "constants.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
